46b98b03bfa63e20822786584064394a83e40ce2
JfmGijsbers <jfmgijsbers@gmail.com>
2020-02-06 19:56:20 +0100

    Removed the ListAdapter functionality, since it didnt work

--- app/src/main/java/com/group02tue/geomeet/Dashboard.java
+++ app/src/main/java/com/group02tue/geomeet/Dashboard.java
@@ -8,18 +8,15 @@ import android.app.Activity;
 
 public class Dashboard extends AppCompatActivity {
 
-    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
-
     ListView simpleList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        simpleList = (ListView) findViewById(R.id.meetingListView);
-        MeetinglistAdapter adapter = new MeetinglistAdapter(getApplicationContext(),
-                countryList);
-        simpleList.setAdapter(adapter);
+        //simpleList = (ListView) findViewById(R.id.meetingListView);
+        //MeetinglistAdapter adapter = new MeetinglistAdapter(getApplicationContext());
+        //simpleList.setAdapter(adapter);
 
     }
 }
--- app/src/main/java/com/group02tue/geomeet/MainActivity.java
+++ app/src/main/java/com/group02tue/geomeet/MainActivity.java
@@ -2,7 +2,9 @@ package com.group02tue.geomeet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
+    }
+    public void toRegister(View view) {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
     }
 }
