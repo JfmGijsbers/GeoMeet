f7c8498db85d097e8eed6b6d5b84ff95e96ab6c9
JfmGijsbers <j.f.m.gijsbers@student.tue.nl>
2020-02-06 14:56:14 +0100

    Made the Dashboard

--- app/src/main/java/com/group02tue/geomeet/Dashboard.java
+++ app/src/main/java/com/group02tue/geomeet/Dashboard.java
@@ -3,12 +3,23 @@ package com.group02tue.geomeet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.app.Activity;
 
 public class Dashboard extends AppCompatActivity {
 
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+
+    ListView simpleList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        simpleList = (ListView) findViewById(R.id.meetingListView);
+        MeetinglistAdapter adapter = new MeetinglistAdapter(getApplicationContext(),
+                countryList);
+        simpleList.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/com/group02tue/geomeet/MeetinglistAdapter.java
@@ -0,0 +1,51 @@
+package com.group02tue.geomeet;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.zip.Inflater;
+
+public class MeetinglistAdapter extends BaseAdapter {
+    Context context;
+    String countryList[];
+    int flags[];
+    LayoutInflater inflater;
+
+    public MeetinglistAdapter(Context applicationContext, String[] countryList) {
+        this.context = context;
+        this.countryList = countryList;
+        this.flags = flags;
+        inflater = (LayoutInflater.from(applicationContext));
+    }
+
+    @Override
+    public int getCount() {
+        return countryList.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflater.inflate(R.layout.activity_meetinglist_item, null);
+        TextView country = (TextView) view.findViewById(R.id.textView);
+        ImageView icon = (ImageView) view.findViewById(R.id.icon);
+        country.setText(countryList[i]);
+        icon.setImageResource(flags[i]);
+        return view;
+    }
+}
